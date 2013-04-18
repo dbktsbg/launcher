68e7efc78d6d5c7bc21ff41e8530035d4247d974
david <dbkdev50@gmail.com>
2013-04-18 15:28:22 -0400

    more debug

--- www/js/launcher.js
+++ www/js/launcher.js
@@ -2,16 +2,19 @@
 function InitializeLauncher() 
 {
     //SetupToLaunchCloudVoteParticipant();
+    alert("InitializeLauncher");
     LaunchParticipantWebApp();
 }
 
 
 function LaunchParticipantWebApp()
 {
+    alert("LaunchParticipantWebApp");
+
     var DeviceID = device.uuid;
     var ARCloudWebAPIAddress = "http://meridiasswebsite.cloudapp.net/Participant/JoinUsingMeridiaDevice";
 
-    alert("ARCloudWebAPIAddress + " / Participant / JoinUsingMeridiaDevice / " + DeviceID");
+    alert("ARCloudWebAPIAddress + " / Participant / JoinUsingMeridiaDevice / " + DeviceI");
 
     open(ARCloudWebAPIAddress + "/Participant/JoinUsingMeridiaDevice/" + DeviceID);
 }
