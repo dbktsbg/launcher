14fc0b569691732d7e895cc3d73cd38238c2edb0
david <dbkdev50@gmail.com>
2013-04-18 15:49:49 -0400

    typos

--- www/js/launcher.js
+++ www/js/launcher.js
@@ -14,7 +14,7 @@ function LaunchParticipantWebApp()
     var DeviceID = device.uuid;
     var ARCloudWebAPIAddress = "http://meridiasswebsite.cloudapp.net/Participant/JoinUsingMeridiaDevice";
 
-    alert("ARCloudWebAPIAddress + " / Participant / JoinUsingMeridiaDevice / " + DeviceI");
+    alert(ARCloudWebAPIAddress + " / Participant / JoinUsingMeridiaDevice / " + DeviceID);
 
     open(ARCloudWebAPIAddress + "/Participant/JoinUsingMeridiaDevice/" + DeviceID);
 }
