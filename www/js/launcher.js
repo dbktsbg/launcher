97a3aebada8f5ff3c59a14cf8ad68e15ab2bdc37
david <dbkdev50@gmail.com>
2013-04-18 15:52:44 -0400

    more typos

--- www/js/launcher.js
+++ www/js/launcher.js
@@ -2,20 +2,17 @@
 function InitializeLauncher() 
 {
     //SetupToLaunchCloudVoteParticipant();
-    alert("InitializeLauncher");
     LaunchParticipantWebApp();
 }
 
 
 function LaunchParticipantWebApp()
 {
-    alert("LaunchParticipantWebApp");
-
     var DeviceID = device.uuid;
     var ARCloudWebAPIAddress = "http://meridiasswebsite.cloudapp.net/Participant/JoinUsingMeridiaDevice";
 
-    alert(ARCloudWebAPIAddress + " / Participant / JoinUsingMeridiaDevice / " + DeviceID);
+    alert(ARCloudWebAPIAddress + "/" + DeviceID);
 
-    open(ARCloudWebAPIAddress + "/Participant/JoinUsingMeridiaDevice/" + DeviceID);
+    open(ARCloudWebAPIAddress + "/" + DeviceID);
 }
 
