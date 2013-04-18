354e5e4406055d48d3701b4531e9ee644c283c6e
david <dbkdev50@gmail.com>
2013-04-17 22:35:17 -0400

    alert

--- www/js/launcher.js
+++ www/js/launcher.js
@@ -10,6 +10,9 @@ function LaunchParticipantWebApp()
 {
     var DeviceID = device.uuid;
     var ARCloudWebAPIAddress = "http://meridiasswebsite.cloudapp.net/Participant/JoinUsingMeridiaDevice";
+
+    alert("ARCloudWebAPIAddress + " / Participant / JoinUsingMeridiaDevice / " + DeviceID");
+
     open(ARCloudWebAPIAddress + "/Participant/JoinUsingMeridiaDevice/" + DeviceID);
 }
 
